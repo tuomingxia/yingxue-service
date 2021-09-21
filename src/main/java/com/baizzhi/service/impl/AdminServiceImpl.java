fcf1e29a8b16c87937ff22582798ca322ce7cb78
mx <1794824640@qq.com>
2021-09-21 14:46:40 +0800

    管理员验证码修改redis存储验证码功能

--- src/main/java/com/baizzhi/service/impl/AdminServiceImpl.java
+++ src/main/java/com/baizzhi/service/impl/AdminServiceImpl.java
@@ -3,26 +3,36 @@ package com.baizzhi.service.impl;
 import com.baizzhi.entity.Admin;
 import com.baizzhi.service.AdminService;
 import com.baizzhi.uitl.ImageCodeUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
 public class AdminServiceImpl implements AdminService {
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+
     @Override
     public HashMap<String,Object> getImageCode() {
 
         //1.生成验证码随机数
        String imgCode=ImageCodeUtil.getSecurityCode();
-        //2.存储验证码随机数
+        //存储验证码随机数
         System.out.println("验证码: "+imgCode);
         //key(唯一)  value验证码随机数
         String codeId= UUID.randomUUID().toString();
+        //2.存储验证码随机数 redis 设置key的存活时间为15分钟
+        stringRedisTemplate.opsForValue().set(codeId,imgCode,15, TimeUnit.MINUTES);
+
 
         HashMap<String,Object>map=new HashMap<>();
 
